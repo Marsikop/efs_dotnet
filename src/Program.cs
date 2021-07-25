7fea4cd1dc85f08c10d07f4e0528d299fb6f213d
Roy <roy@negital.com>
2021-07-25 14:03:59 +0300

    init

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.StructuredLogging.Json;
+
+
+namespace Playgoriund
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            try
+            {
+                logger.ExtendedInfo("Sending order", new { OrderId = 1234, RestaurantId = 4567 });
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                // NLog: catch any exception and log it.
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+        }
+    }
+}
